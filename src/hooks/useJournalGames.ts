45361480b1834a004ef78eed714bfef329fa6be7
Agustin Escobar <agusesgi@gmail.com>
2025-11-27 20:16:02 -0300

    Add useJournalGames hook

--- src/hooks/useJournalGames.ts
+++ src/hooks/useJournalGames.ts
@@ -3,7 +3,7 @@ import { getAllGames } from "../api/game.api";
 
 export function useAllGames(page = 1) {
   return useQuery({
-    queryKey: ["games", page],
+    queryKey: ["journalGames", page],
     queryFn: () => getAllGames(page, 54),
   });
 }
