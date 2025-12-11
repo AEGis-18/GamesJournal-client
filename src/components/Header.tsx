110a473c9031dd677ae2c9ff97dce654fd893fd5
Agustin Escobar <agusesgi@gmail.com>
2025-12-11 18:32:30 -0300

    Change logo location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,13 @@ export default function Header() {
     <div className="grid grid-cols-2 mb-2 bg-neutral-900 relative z-40">
       <header className="flex justify-start -skew-x-12 mx-2">
         <Link to="/" className="hover:bg-white hover:text-black p-2 m-0 ">
-          <img
-            src="./src/assets/logo-alt.svg"
-            className="h-10 w-10 skew-x-12"
-          />
+          <img src="/logo-alt.svg" className="h-10 w-10 skew-x-12" />
         </Link>
       </header>
       <nav className="flex justify-end -skew-x-12 mr-2">
         <Link
           to="/games"
-          activeOptions={{ exact: true }}
+          activeOptions={{ exact: false }}
           className="[&.active]:bg-blue-500  [&.active]:text-white hover:bg-white hover:text-black p-4"
         >
           <h2 className="skew-x-12">Games</h2>
